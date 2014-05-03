422044b99da58fd0cd917de03afc8d931cf6115e
Simon <chensimon@outlook.com>
2014-05-03 10:26:04 -0400

    working mongofb

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, session, request
+from pymongo import MongoClient
 import auth
 
 app = Flask(__name__)
@@ -9,7 +10,7 @@ def index():
 	
 @app.route("/login",methods=["GET","POST"])
 def login():
-	if request.method = "GET":
+	if request.method == "GET":
 		return render_template('login.html')
 	else:
 		username = request.form['username'].encode('ascii','ignore')
@@ -17,17 +18,20 @@ def login():
 		if auth.login(username,password):
 			session['username'] = username
 		else:
-			return render_template('login.html', message = auth.login(username,password))
+			return redirect('/login', message = auth.login(username,password))
 		
 @app.route("/register",methods=["GET","POST"])
 def register():
-	if request.method = "GET":
+	if request.method == "GET":
 		return render_template('register.html')
 	else:
 		username = request.form['username'].encode('ascii','ignore')
-		password = request.form['password'].encode('ascii','ignore')	
-		if auth.register(username,password) == 'True':
-			return render_template('login.html',message="Account made")
+		password = request.form['password'].encode('ascii','ignore')
+                print username
+                print password
+		if auth.register(username,password):
+                        print "Auth.register worked!"
+			return redirect('/login')
 		else:
 			return render_template('register.html',message= auth.register(username,password))
 			
--- auth.py
+++ auth.py
@@ -1,27 +1,51 @@
-import sqlite3
+from pymongo import MongoClient
 
-def conn(db):
-	conn = sqlite3.connect(db)
-	c = conn.cursor()
-	if db == 'users.db':
-		c.execute(CREATE TABLE if not exists users (username text,password text))
-		conn.commit
-	return [conn,c]
+client = MongoClient()
+db = client.pcparts
 	
-def register(username, password):
-	c = conn('users.db')
-	c[1].execute('Select * from users where username=? ',(username,))
-	if c[1].rowcount > 0:
-		return "User already exists"
-	else:
-		c[1].execute('INSERT into users VALUES (?,?)', (username,password))
+# def register(username, password):
+# 	conn = Dconn('users.db')
+#         c = conn.cursor()
+# 	c.execute('Select * from users where username= ? ',(username,))
+#         check = c.fetchone();
+# 	if check is not None:
+# 		return "User already exists"
+# 	else:
+# 		c.execute('INSERT into users VALUES (?,?)', (username,password))
+#                 conn.commit
+#                 return True
 	
-def login(username,password):
-	c = conn('users.db')
-	c[1].execute('Select * from users where username=? where password=?',(username,password))
-	if c[1].rowcount > 0:
-		return "Incorrect Info."
-	else:
-		return True;
+# def login(username,password):
+# 	c = conn('users.db')
+# 	c[1].execute('Select * from users where username=? where password=?',(username,password))
+# 	if c[1].rowcount > 0:
+# 		return "Incorrect Info."
+# 	else:
+# 		return True
+
+def register(username,password):
+        if(db.users.find({"username":username}).count()) != 0:
+                return "There is a account with that username"
+        elif(chkpw != password):
+                return "Passwords aren't the same"
+        else:
+                db.users.insert({'username':username, 'password' : password})
+                return True
+
+def login(user,password):
+        check=db.users.find_one({'username':user,'password':password}, fields={'_id':False})
+        if (db.users.find({"username":user}).count()) == 0:
+                return "No account with that username"
+        elif check == None:
+                return "Username or password is invalid"
+        else:
+                return True
+
+def add(username,school,classes,time):
+        user = db.users.update({'username':username},{$addToSet: {tags: {$each ["schools","class","time"]}}
+                             }
+        
+        
+        
 	
 	
